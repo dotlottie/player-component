29cefe1fbdb7df7e5607ef5aab44cebecedb9b0c
Karam <karam7898@gmail.com>
2020-08-06 16:23:48 +0800

    rollup config, typings generation

---
+++ rollup.config.js
@@ -0,0 +1,71 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+import filesize from 'rollup-plugin-filesize';
+import resolve from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+import typescript2 from 'rollup-plugin-typescript2';
+
+const production = !process.env.ROLLUP_WATCH;
+const extensions = ['.js', '.jsx', '.ts', '.tsx', '.mjs'];
+const outputDir = './dist/';
+
+export default {
+  input: './src/dotlottie-player.ts',
+  treeshake: !!production,
+  output: [
+    {
+      file: './dist/dotlottie-player.esm.js',
+      // dir: outputDir,
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: './dist/dotlottie-player.js',
+      format: 'umd',
+      name: 'dotlottie-player',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve({
+      extensions,
+      jsnext: true,
+      module: true,
+    }),
+    commonjs(),
+    typescript2({
+      check: false,
+    }),
+    babel({
+      extensions: extensions,
+      exclude: ['./node_modules/**'],
+    }),
+    !production &&
+      copy({
+        targets: [
+          { src: './src/index.html', dest: outputDir },
+          { src: './src/sample_dotlottie.lottie', dest: outputDir },
+          {
+            src: './node_modules/@webcomponents/webcomponentsjs/bundles/',
+            dest: outputDir,
+          },
+          {
+            src: './node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js',
+            dest: outputDir,
+          },
+        ],
+      }),
+    filesize(),
+    !production &&
+      serve({
+        contentBase: [outputDir],
+        open: true,
+        host: 'localhost',
+        port: 10000,
+      }),
+
+    production && terser(),
+  ],
+};
--- src/dotlottie-player.ts
+++ src/dotlottie-player.ts
@@ -1,6 +1,6 @@
 import { customElement, LitElement, html, property, query, TemplateResult } from 'lit-element';
 import * as lottie from 'lottie-web/build/player/lottie_svg';
-import * as JSZip from 'jszip';
+import JSZip from 'jszip/dist/jszip';
 
 import styles from './dotlottie-player.styles';
 
@@ -11,13 +11,13 @@ export enum PlayerState {
   Paused = 'paused',
   Stopped = 'stopped',
   Frozen = 'frozen',
-  Error = 'error'
+  Error = 'error',
 }
 
 // Define play modes
 export enum PlayMode {
   Normal = 'normal',
-  Bounce = 'bounce'
+  Bounce = 'bounce',
 }
 
 // Define player events
@@ -31,7 +31,7 @@ export enum PlayerEvents {
   Freeze = 'freeze',
   Loop = 'loop',
   Complete = 'complete',
-  Frame = 'frame'
+  Frame = 'frame',
 }
 
 /**
@@ -43,11 +43,13 @@ export function fetchPath(path: string): Promise<string> {
     xhr.open('GET', path, true);
     xhr.responseType = 'arraybuffer';
     xhr.send();
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
         JSZip.loadAsync(xhr.response)
           .then((zip: JSZip) => {
-            zip.file('manifest.json').async('string')
+            zip
+              .file('manifest.json')
+              .async('string')
               .then((manifestFile: string) => {
                 const manifest = JSON.parse(manifestFile);
 
@@ -61,7 +63,9 @@ export function fetchPath(path: string): Promise<string> {
 
                 const defaultLottie = manifest.animations[0];
 
-                zip.file(`animations/${defaultLottie.id}.json`).async('string')
+                zip
+                  .file(`animations/${defaultLottie.id}.json`)
+                  .async('string')
                   .then((lottieFile: string) => {
                     const lottieJson = JSON.parse(lottieFile);
 
@@ -72,15 +76,18 @@ export function fetchPath(path: string): Promise<string> {
                             return;
                           }
 
-                          return new Promise((resolveAsset) => {
-                            zip.file(`images/${asset.p}`).async('base64').then((assetB64: any) => {
-                              asset.p = 'data:;base64,' + assetB64;
-                              asset.e = 1;
+                          return new Promise(resolveAsset => {
+                            zip
+                              .file(`images/${asset.p}`)
+                              .async('base64')
+                              .then((assetB64: any) => {
+                                asset.p = 'data:;base64,' + assetB64;
+                                asset.e = 1;
 
-                              resolveAsset();
-                            });
+                                resolveAsset();
+                              });
                           });
-                        })
+                        }),
                       ).then(() => {
                         resolve(lottieJson);
                       });
@@ -213,7 +220,7 @@ export class DotLottiePlayer extends LitElement {
       return;
     }
 
-    const frame: number = ((e.target.value / 100) * this._lottie.totalFrames);
+    const frame: number = (e.target.value / 100) * this._lottie.totalFrames;
 
     this.seek(frame);
   }
@@ -251,7 +258,7 @@ export class DotLottiePlayer extends LitElement {
       // Initialize lottie player and load animation
       this._lottie = lottie.loadAnimation({
         ...options,
-        animationData: srcParsed
+        animationData: srcParsed,
       });
     } catch (err) {
       this.currentState = PlayerState.Error;
@@ -265,12 +272,14 @@ export class DotLottiePlayer extends LitElement {
       this._lottie.addEventListener('enterFrame', () => {
         this.seeker = (this._lottie.currentFrame / this._lottie.totalFrames) * 100;
 
-        this.dispatchEvent(new CustomEvent(PlayerEvents.Frame, {
-          detail: {
-            frame: this._lottie.currentFrame,
-            seeker: this.seeker
-          }
-        }));
+        this.dispatchEvent(
+          new CustomEvent(PlayerEvents.Frame, {
+            detail: {
+              frame: this._lottie.currentFrame,
+              seeker: this.seeker,
+            },
+          }),
+        );
       });
 
       // Handle animation play complete
@@ -366,7 +375,7 @@ export class DotLottiePlayer extends LitElement {
    */
   public play() {
     if (!this._lottie) {
-      return
+      return;
     }
 
     this._lottie.play();
@@ -380,7 +389,7 @@ export class DotLottiePlayer extends LitElement {
    */
   public pause(): void {
     if (!this._lottie) {
-      return
+      return;
     }
 
     this._lottie.pause();
@@ -394,7 +403,7 @@ export class DotLottiePlayer extends LitElement {
    */
   public stop(): void {
     if (!this._lottie) {
-      return
+      return;
     }
 
     this._counter = 0;
@@ -419,9 +428,7 @@ export class DotLottiePlayer extends LitElement {
     }
 
     // Calculate and set the frame number
-    const frame = matches[2] === '%'
-      ? this._lottie.totalFrames * Number(matches[1]) / 100
-      : matches[1];
+    const frame = matches[2] === '%' ? (this._lottie.totalFrames * Number(matches[1])) / 100 : matches[1];
 
     // Set seeker to new frame number
     this.seeker = frame;
@@ -445,7 +452,7 @@ export class DotLottiePlayer extends LitElement {
 
     // Get SVG element and serialize markup
     const svgElement = this.shadowRoot.querySelector('.animation svg') as Node;
-    const data = (new XMLSerializer()).serializeToString(svgElement);
+    const data = new XMLSerializer().serializeToString(svgElement);
 
     // Trigger file download
     if (download) {
@@ -469,7 +476,7 @@ export class DotLottiePlayer extends LitElement {
    */
   private freeze(): void {
     if (!this._lottie) {
-      return
+      return;
     }
 
     this._lottie.pause();
@@ -520,9 +527,7 @@ export class DotLottiePlayer extends LitElement {
    * Toggle playing state.
    */
   public togglePlay(): void {
-    return this.currentState === PlayerState.Playing
-      ? this.pause()
-      : this.play();
+    return this.currentState === PlayerState.Playing ? this.pause() : this.play();
   }
 
   /**
@@ -592,21 +597,39 @@ export class DotLottiePlayer extends LitElement {
       <div class="toolbar">
         <button @click=${this.togglePlay} class=${isPlaying || isPaused ? 'active' : ''}>
           ${isPlaying
-        ? html`<svg width="24" height="24"><path d="M14.016 5.016H18v13.969h-3.984V5.016zM6 18.984V5.015h3.984v13.969H6z"/></svg>`
-        : html`<svg width="24" height="24"><path d="M8.016 5.016L18.985 12 8.016 18.984V5.015z"/></svg>`
-      }
+            ? html`
+                <svg width="24" height="24">
+                  <path d="M14.016 5.016H18v13.969h-3.984V5.016zM6 18.984V5.015h3.984v13.969H6z" />
+                </svg>
+              `
+            : html`
+                <svg width="24" height="24"><path d="M8.016 5.016L18.985 12 8.016 18.984V5.015z" /></svg>
+              `}
         </button>
         <button @click=${this.stop} class=${isStopped ? 'active' : ''}>
           <svg width="24" height="24"><path d="M6 6h12v12H6V6z" /></svg>
         </button>
-        <input class="seeker" type="range" min="0" step="1" max="100" .value=${this.seeker}
+        <input
+          class="seeker"
+          type="range"
+          min="0"
+          step="1"
+          max="100"
+          .value=${this.seeker}
           @input=${this._handleSeekChange}
-          @mousedown=${() => { this._prevState = this.currentState; this.freeze(); }}
-          @mouseup=${() => { this._prevState === PlayerState.Playing && this.play(); }}
+          @mousedown=${() => {
+            this._prevState = this.currentState;
+            this.freeze();
+          }}
+          @mouseup=${() => {
+            this._prevState === PlayerState.Playing && this.play();
+          }}
         />
         <button @click=${this.toggleLooping} class=${this.loop ? 'active' : ''}>
           <svg width="24" height="24">
-            <path d="M17.016 17.016v-4.031h1.969v6h-12v3l-3.984-3.984 3.984-3.984v3h10.031zM6.984 6.984v4.031H5.015v-6h12v-3l3.984 3.984-3.984 3.984v-3H6.984z"/>
+            <path
+              d="M17.016 17.016v-4.031h1.969v6h-12v3l-3.984-3.984 3.984-3.984v3h10.031zM6.984 6.984v4.031H5.015v-6h12v-3l3.984 3.984-3.984 3.984v-3H6.984z"
+            />
           </svg>
         </button>
       </div>
@@ -618,10 +641,15 @@ export class DotLottiePlayer extends LitElement {
 
     return html`
       <div class=${'main ' + className}>
-        <div class="animation" style=${'background:' + this.background }>
-          ${this.currentState === PlayerState.Error ? html`<div class="error">⚠</div>` : undefined}
+        <div class="animation" style=${'background:' + this.background}>
+          ${this.currentState === PlayerState.Error
+            ? html`
+                <div class="error">⚠</div>
+              `
+            : undefined}
         </div>
         ${this.controls ? this.renderControls() : undefined}
-      </div>`
+      </div>
+    `;
   }
 }
