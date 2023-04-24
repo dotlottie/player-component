bf8f865209f1b1ec3e0c3458d0d2948667a16dba
samuelOsborne <samu.osborne@gmail.com>
2023-04-24 18:25:11 +0200

    feat: v2 init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,54 @@
+/**
+ * Copyright 2022 Design Barn Inc.
+ */
+
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  root: true,
+
+  plugins: ['@lottiefiles'],
+
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@lottiefiles/esnext',
+    'plugin:@lottiefiles/typescript',
+    'plugin:@lottiefiles/typescript-typechecking',
+    'plugin:@lottiefiles/nodejs',
+    'plugin:@lottiefiles/prettier',
   ],
-  parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
+
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.cts', '*.mts'],
+
+      parserOptions: {
+        project: ['./packages/*/tsconfig.json', './tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
     },
-  },
+
+    // Allow code in bin and scripts folders to do CLI things
+    {
+      files: ['**/@(bin|scripts|examples)/**/*.@(js|ts|cjs|mjs|cts|mts)'],
+      rules: {
+        'no-console': 'off',
+        'node/shebang': 'off',
+      },
+    },
+  ],
+
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
+    'default-param-last': 'off',
+    '@typescript-eslint/default-param-last': ['error'],
+    '@typescript-eslint/prefer-for-of': 'off',
+    '@typescript-eslint/dot-notation': 'off',
+    '@typescript-eslint/member-ordering': 'off',
+
+    // Disable the async generator warning
+    'no-restricted-syntax': 'off',
+
+    // Disable deprecation testing
+    'deprecation/deprecation': 'off',
+
+    'no-undefined': 'off',
+    'import/extensions': 'off',
   },
 };
