22feea1f5a97fca0520a283c990db879194f7979
Afsal <128362946+afsal-ali@users.noreply.github.com>
2023-05-30 20:30:34 +0800

    fix(react): moved PlayerEvents to common package (#102)

--- packages/react-player/src/index.ts
+++ packages/react-player/src/index.ts
@@ -2,12 +2,12 @@
  * Copyright 2023 Design Barn Inc.
  */
 
-import { PlayMode, PlayerState } from 'common';
+import { PlayMode, PlayerState, PlayerEvents } from 'common';
 import type { Manifest, ManifestAnimation } from 'common';
 
 import type { DotLottieRefProps } from './hooks/use-dotlottie-player';
 
-export { PlayMode, PlayerState, DotLottieRefProps };
+export { PlayerEvents, PlayMode, PlayerState, DotLottieRefProps };
 export type { Manifest, ManifestAnimation };
 export * from './react-player';
 export * from './controls';
