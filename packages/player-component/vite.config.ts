22f4481ef292dd8c09653c601c3e07889fb45ecc
Afsal <afsal@lottiefiles.com>
2023-06-08 15:01:52 +0800

    fix(player-component): vite.config cleanup

--- packages/player-component/vite.config.ts
+++ packages/player-component/vite.config.ts
@@ -2,10 +2,8 @@
  * Copyright 2023 Design Barn Inc.
  */
 
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
 });
