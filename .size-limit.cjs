67d75c6ac852cc8e3d6db58fb91c0b7b00ae47d9
Abdelrahman Ashraf <a.theashraf@gmail.com>
2023-08-17 06:18:58 +0800

    ci: 🎡 verify bundle size

---
+++ .size-limit.cjs
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    path: 'packages/common/dist/index.js',
+  },
+  {
+    path: 'packages/player-component/dist/dotlottie-player.js',
+  },
+  {
+    path: 'packages/react-player/dist/index.js',
+  },
+];
