5b3c7d81597b6f1cfcf259376204e6a0a56e01ce
Abdelrahman Ashraf <a.theashraf@gmail.com>
2023-08-17 10:38:16 +0800

    ci: 🎡 report bundle size

---
+++ .size-limit.cjs
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    path: 'packages/common/dist/index.js',
+  },
+  {
+    path: 'packages/player-component/dist/dotlottie-player.mjs',
+  },
+  // {
+  //   path: 'packages/react-player/dist/index.js',
+  // },
+];
