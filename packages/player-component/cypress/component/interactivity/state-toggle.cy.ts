ca0f7e6faec3c97405d00b22e1f4a59144020029
Afsal <afsal@lottiefiles.com>
2023-08-22 19:51:48 +0800

    feat: 🎸 added interactivity tests
    ✅ Closes: added interactivity tests

---
+++ packages/player-component/cypress/component/interactivity/state-toggle.cy.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2023 Design Barn Inc.
+ */
+
+import { PlayerState } from '@dotlottie/common';
+import { html } from 'lit';
+
+describe('Interactivity: state_toggle (onClick)', () => {
+  it('should be able set activeStateId state_toggle', () => {
+    cy.mount(
+      html`
+        <dotlottie-player activeStateId="state_toggle" data-testid="testPlayer" autoplay loop controls style="height: 200px;" src="/lf_interactivity_page.lottie">
+        </dotlottie-player>
+      `,
+    );
+
+    cy.get('[name="currentState"]').should('have.value', PlayerState.Paused);
+    cy.get('[name="currentState"]').should('not.have.value', PlayerState.Playing);
+    cy.get('[name="activeStateId"]').should('have.value', "state_toggle");
+  });
+
+  it('should transition on click', () => {
+    cy.mount(
+      html`
+        <dotlottie-player activeStateId="state_toggle" data-testid="testPlayer" autoplay loop controls style="height: 200px;" src="/lf_interactivity_page.lottie">
+        </dotlottie-player>
+      `,
+    );
+
+    // eslint-disable-next-line no-warning-comments
+    // TODO: increase animation speed. to make the tests faster.
+  
+    // State: startIdle
+    cy.get('[name="currentState"]').should('have.value', PlayerState.Paused);
+    cy.get('[name="frame"]').should('have.value', 0);
+
+    // State: playSun
+
+    cy.get('[data-testid="testPlayer"]').shadow().find('.animation').click();
+    cy.get('[name="currentState"]').should('have.value', PlayerState.Playing);
+    cy.get('[name="loop"]').should('have.value', "false");
+    cy.get('[name="autoplay"]').should('have.value', "false");
+    cy.get('[name="frame"]').should('have.value', 29);
+
+    // State: playReverse
+    cy.get('[data-testid="testPlayer"]').shadow().find('.animation').click();
+    cy.get('[name="currentState"]').should('have.value', PlayerState.Playing);
+    cy.get('[name="loop"]').should('have.value', "false");
+    cy.get('[name="autoplay"]').should('have.value', "true");
+    cy.get('[name="frame"]').should('have.value', 0);
+  });
+});
