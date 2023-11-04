5efc8c87c025928a9d4a41725e93b635edfec4d8
yash1200 <yashjohri1200@gmail.com>
2023-11-04 22:01:45 +0530

    fix: change off to warn for prefer-object-spread

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
     '@typescript-eslint/prefer-for-of': 'off',
     '@typescript-eslint/dot-notation': 'off',
     '@typescript-eslint/member-ordering': 'off',
-    'prefer-object-spread': 'off',
+    'prefer-object-spread': 'warn',
 
     // Disable the async generator warning
     'no-restricted-syntax': 'off',
